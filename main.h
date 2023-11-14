d73b4db4a20e1fde556f24a60ee7dcfa42e3b912
tr-wa2el <60959124+tr-wa2el@users.noreply.github.com>
2023-11-14 14:31:11 +0200

    Update main.h

--- main.h
+++ main.h
@@ -2,9 +2,6 @@
 #define _MAIN_
 
 int _putchar(char);
-int output_character(char c);
-int output_string(const char *s);
-int process_format_specifier(const char **format_pointer, va_list args);
 int _printf(const char *format, ...);
 
 #endif
